1d6b4abbabae6818dae37c1b6c8658f2584f6c65
Vannesa <vannehifu2020@gmail.com>
2021-08-02 12:43:44 -0500

    Organizar la parte de inicio de sesion

--- src/app/routes/inventory.js
+++ src/app/routes/inventory.js
@@ -4,16 +4,23 @@ const pool = require('../../config/db')
 
 module.exports = app => {
     app.get('/', (req, res) => {
-        pool.query('SELECT * FROM users', (err, result) => {
+        /*pool.query('SELECT * FROM users', (err, result) => {
             console.log(result);
             res.render("../views/index.ejs", {
                 inventario: result
 
             })
-        })
+        })*/
+        if(req.session.loggedin){
+            res.render("../views/index.ejs")
+        }else{
+            res.render("../views/login.ejs")
+        }
     })
-    app.get('/login', (req, res) => {
-        res.render('../views/login.ejs')
+    app.get('/logout', (req, res) => {
+       req.session.destroy(()=>{
+           res.redirect("/")
+       })
     })
     app.get('/registro', (req, res) => {
         res.render('../views/registro.ejs')
@@ -25,7 +32,7 @@ module.exports = app => {
     app.post('/registro', async(req, res) => {
         const { nombre, apellido, correo, telefono, pass, subject} = req.body
 
-        console.log(req.body);
+        /*console.log(req.body);*/
 
         const newUser = {
             nombre,
@@ -47,11 +54,11 @@ module.exports = app => {
         if(email && password){
             pool.query('SELECT * FROM usuarios2 WHERE correo = ?', [email], async (error,result) => {
                 if(result.length === 0 || !(password === result[0].pass)){
-                    res.render("../views/login.ejs")
+                    res.redirect("/")
                 }else{
                     req.session.loggedin=true; 
                     req.session.name=result[0].nombre 
-                    res.render("../views/index.ejs")
+                    res.redirect("/")
                 }
             })
 
